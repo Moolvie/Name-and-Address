560d862c8b65b6b091c78fef5356bc9774848da8
Moolvie <moolvie@gmail.com>
2017-03-02 15:14:19 -0500

    First commit

---
+++ NameAndAddress/Form1.vb
@@ -0,0 +1,15 @@
+﻿Public Class Form1
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        ' Close the program
+        Me.Close()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnShowInfo.Click
+        ' Display my Address
+        lblName.Visible = True
+        lblStreet.Visible = True
+        lblCityStateZip.Visible = True
+
+    End Sub
+End Class
